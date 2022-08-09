6ac6a8f094f5491ff7997190a9a4192d5aa39cab
Scott Sirri <scottsirri@wustl.edu>
2022-08-09 12:55:56 -0500

    Added selector to y_vars conversion. Might switch to PuLP rather than SciPY

--- sel.py
+++ sel.py
@@ -8,7 +8,7 @@ class Selector:
     family = []
 
     def __init__(self, in_n):
-        n = in_n
+        self.n = in_n
 
     def validate():
         if n <= 0:
@@ -21,6 +21,38 @@ class Selector:
                     return INVALID
         return VALID
 
+sel = Selector(3)
+
+# Test input
+sel.family.append([1,2])
+sel.family.append([2])
+sel.family.append([1,3])
+print(sel.n)
+print(sel.family)
+
+z_vars = []
+x_vars = []
+D_vars = []
+c_vars = []
+d_vars = []
+
+# Initializing the instance (y variables)
+y_vars = []
+for sel_set in sel.family:
+    set_i_list = []
+    for i in range(sel.n):
+        if i+1 in sel_set:
+            set_i_list.append(1)
+        else:
+            set_i_list.append(0)
+    y_vars.append(set_i_list)
+
+for sel_list in y_vars:
+    print(sel_list)
+
+
+
+"""
 obj = [-1, -2]
 
 lhs_ineq = [[ 2,  1],
@@ -42,5 +74,5 @@ opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
 
 opt
 print(opt)
-
+"""
 print("Success")
