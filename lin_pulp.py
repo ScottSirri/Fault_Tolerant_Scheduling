1275f29c1bb840b56d43a10f7a0fb2442835b3d4
Scott Sirri <scottsirri@wustl.edu>
2022-08-09 13:11:15 -0500

    Set up PuLP, seems more promising than SciPY

--- lin_pulp.py
+++ lin_pulp.py
@@ -1,11 +1,26 @@
 from pulp import LpMaximize, LpMinimize, LpProblem, LpStatus, lpSum, LpVariable
 
-model = LpProblem(name="small-problem", sense=LpMinimize)
+model = LpProblem(name="small-problem", sense=LpMaximize)
 
-x = LpVariable(name="x", lowBound=0)
+x = LpVariable(name="x", lowBound=0, cat = "Integer")
 y = LpVariable(name="y", lowBound=0)
 
-expression = 2 * x + 4 * y
-type(expression)
+model += (2 * x + y <= 20, "red_constraint")
+model += (4 * x - 5 * y >= -10, "blue_constraint")
+model += (-x + 2 * y >= -2, "yellow_constraint")
+model += (-x + 5 * y == 15, "green_constraint")
+
+# Objective function
+model += lpSum([x, 2*y])
+
+print(model)
+status = model.solve()
+
+print(f"status: {model.status}, {LpStatus[model.status]}")
+
+print(f"objective: {model.objective.value()}")
+
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
 
 print("Success")
