48478e08ff73f505a4b8d58a3b284ff1ffbc055a
Scott Sirri <scottsirri@wustl.edu>
2022-08-09 14:20:45 -0500

    Implementing variable generation. Changed indexing from 1-index to 0-index on vertices, lower chance of off-by-one error

--- sel.py
+++ sel.py
@@ -1,4 +1,4 @@
-from scipy.optimize import linprog
+from pulp import LpMaximize, LpMinimize, LpProblem, LpStatus, lpSum, LpVariable
 
 VALID = 0
 INVALID = -1
@@ -17,16 +17,17 @@ class Selector:
             if type(set) is not list:
                 return INVALID
             for i in set:
-                if i <= 0 or i > n:
+                if i < 0 or i >= n:
                     return INVALID
         return VALID
 
-sel = Selector(3)
+sel = Selector(4)
 
 # Test input
 sel.family.append([1,2])
 sel.family.append([2])
 sel.family.append([1,3])
+sel.family.append([0,2])
 print(sel.n)
 print(sel.family)
 
@@ -41,38 +42,54 @@ y_vars = []
 for sel_set in sel.family:
     set_i_list = []
     for i in range(sel.n):
-        if i+1 in sel_set:
+        if i in sel_set:
             set_i_list.append(1)
         else:
             set_i_list.append(0)
     y_vars.append(set_i_list)
 
-for sel_list in y_vars:
-    print(sel_list)
+# LP model
+model = LpProblem(name="small-problem", sense=LpMaximize)
+
+for v in range(sel.n):
+    z_vars.append(LpVariable(name = f"{v}", lowBound = 0))
+    x_vars.append(LpVariable(name = f"{v}", lowBound = 0))
+    D_vars.append(LpVariable(name = f"{v}", lowBound = 0))
+    c_vars.append(LpVariable(name = f"{v}", lowBound = 0))
+
+for i in range(len(sel.family)):
+    d_i = []
+    for v in range(sel.n):
+        d_i.append(LpVariable(name = f"d{i},{v}", lowBound = 0))
+    d_vars.append(d_i)
+    
+
+# = LpVariable(name="", lowBound=0)
 
 
 
-"""
-obj = [-1, -2]
+# Constraint: D_{v} = 1
 
-lhs_ineq = [[ 2,  1],
-            [-4,  5],
-            [ 1, -2]]
-rhs_ineq = [20,
-            10,
-             2]
+# Constraint: 0 <= D_{v} - ... <= 2/3
 
-lhs_eq = [[-1, 5]]
-rhs_eq = [15]
+#Constraint: -1 + 1/F <= c_{v} - ... <= 0
 
-bnd = [(0, float("inf")),
-       (0, float("inf"))]
+#Constraint: 0 <= d_{i,v} - ... <= 1 - 1/S_{i}
 
-opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
-              A_eq=lhs_eq, b_eq=rhs_eq, bounds=bnd,
-              method="highs")
 
-opt
-print(opt)
-"""
+model += (2 * x + y <= 20, "red_constraint")
+
+# Objective function
+model += lpSum([])
+
+print(model)
+status = model.solve()
+
+print(f"status: {model.status}, {LpStatus[model.status]}")
+
+print(f"objective: {model.objective.value()}")
+
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
+
 print("Success")
